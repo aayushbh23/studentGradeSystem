29939ec13fc0502134191a395551d28331af0c14
Ayush Bhandari <ayush.bhandari@cqumail.com>
2025-03-25 19:16:57 +1100

    clear button functionality and error handling

--- src/main/java/com/mycompany/studentgradesystem/PrimaryController.java
+++ src/main/java/com/mycompany/studentgradesystem/PrimaryController.java
@@ -3,37 +3,71 @@ package com.mycompany.studentgradesystem;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class PrimaryController implements IExitable{
 
     // Primary Controller Code
+    @FXML
+    private TextArea outputTextArea;
     
-    TextArea outputTextArea = this.outputTextArea;
-    //outputTextArea.setWrapText(true);
+    @FXML
+    private TextField findStudentsIdTextArea;
+    
+    @FXML
+    private TextField resultsInMarkRangeFromTextArea;
+    
+    @FXML
+    private TextField resultsInMarkRangeToTextArea;
+    
     
     @FXML
     private void displayAllGrades(ActionEvent event){
-        // 
+        this.outputTextArea.setText("Invalid Input");
     } 
     
     @FXML
     private void findStudentId(ActionEvent event){
-        // 
+        try{
+            if (this.findStudentsIdTextArea.getText().isEmpty()){
+                this.outputTextArea.setText("No Input Student ID");
+            }else{
+               int studentId = Integer.parseInt(this.findStudentsIdTextArea.getText()); 
+               this.outputTextArea.setText("Student ID: "+studentId);
+            }
+        }
+        catch(NumberFormatException ex){
+           this.outputTextArea.setText("Invalid Input");
+        }
     } 
     
     @FXML
     private void resultsInMarkRange(ActionEvent event){
-        // 
+        try{
+            if (this.resultsInMarkRangeFromTextArea.getText().isEmpty() || this.resultsInMarkRangeToTextArea.getText().isEmpty()){
+                this.outputTextArea.setText("Mark Range is Invalid");
+            }else{
+               int markRangeFrom = Integer.parseInt(this.resultsInMarkRangeFromTextArea.getText()); 
+               int markRangeTo = Integer.parseInt(this.resultsInMarkRangeToTextArea.getText());
+               this.outputTextArea.setText("Mark Range From: "+markRangeFrom +" To: "+markRangeTo);
+            }
+        }
+        catch(NumberFormatException ex){
+           this.outputTextArea.setText("Invalid Input");
+        }         
     } 
     
     @FXML
     private void displayStatistics(ActionEvent event){
-        // 
+        this.outputTextArea.setText("Invalid Input");
     } 
     
     @FXML
     private void clearButton(ActionEvent event){
-        outputTextArea.clear();
+        this.outputTextArea.clear();
+        this.findStudentsIdTextArea.clear();
+        this.resultsInMarkRangeFromTextArea.clear();
+        this.resultsInMarkRangeToTextArea.clear();    
     } 
     
     @FXML
